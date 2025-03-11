152c01f3020897e0c4b67fb8d7987a50e6f9fc25
di207sha <dishalunagariya09@gmail.com>
2025-03-11 15:09:55 +0530

    Front End complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from '../component/SignIn';
 import SignUp from '../component/SignUp';
 import AppTheme from "../component/AppTheme";
+import Home from '../component/Home';
+
 
 function App() {
-  return (
+  return ( 
     <AppTheme> {/* ✅ Wrap the entire router inside AppTheme */}
       <Router>
         <Routes>
-          <Route path="/" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn/>}></Route>
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
